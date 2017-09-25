13ca2c8a607fd9688ae59c0e92934e1e4ceb6c61
laixiangran <laixiangran@163.com>
2017-09-25 16:43:19 +0800

    新增esri.css动态生成

--- example/app/app.component.ts
+++ example/app/app.component.ts
@@ -1,9 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { EssenceNg2EsriMapComponent } from '../../src/essence-ng2-esrimap.component';
-import { Observable } from 'rxjs/Observable';
-import { Http, Response, Headers, RequestOptions } from '@angular/http';
-import 'rxjs/add/operator/catch';
-import 'rxjs/add/operator/map';
 import { environment } from '../environments/environment';
 
 @Component({
@@ -13,15 +9,13 @@ import { environment } from '../environments/environment';
 })
 export class AppComponent implements OnInit {
 	esriMap: EssenceNg2EsriMapComponent;
-	ClusterLayer: any;
 	mapUrl: string[] = ['vec', 'cva'];
 	mapType: string = 'tdt';
 	geoUrl: string = environment.geoUrl; // 几何服务路径
 	gisApiUrl: string = environment.gisApiUrl; // arcgis javascript API路径
 	esriCSSUrl: string = environment.esriCSSUrl; // arcgis javascript API路径
-	clusterLayer: any;
 
-	constructor(public http: Http) {
+	constructor() {
 	}
 
 	ngOnInit() {
